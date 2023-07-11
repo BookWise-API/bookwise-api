d734655077d5abf0bdf3b399cb123e0b0f6c4fc3
carolinalmeidas <carolinalmeida.s@hotmail.com>
2023-07-11 14:55:28 -0300

    fix: serializer books and copies

--- books/serializers.py
+++ books/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Book
-from users.serializers import NormalUserSerializer
+from copies.serializers import CopieSerializer
 from copies.models import Copie
 
 
@@ -10,7 +10,8 @@ class BookSerializer(serializers.ModelSerializer):
         fields = ["id", "title", "author", "description", "number_of_copies"]
 
     def create(self, validated_data):
-        number_of_copies = validated_data.pop("number_of_copies", 0)
+        number_of_copies = validated_data.get("number_of_copies", 0)
+        print(number_of_copies)
         book = super().create(validated_data)
         for _ in range(number_of_copies):
             Copie.objects.create(book=book)
--- copies/views.py
+++ copies/views.py
@@ -11,17 +11,3 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 class CopieListView(generics.ListAPIView):
     queryset = Copie.objects.all()
     serializer_class = CopieSerializer
-    
-class CopieView(generics.CreateAPIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    queryset = Copie
-    serializer_class = CopieSerializer
-
-    def create(self, request: Request, *args, **kwargs):
-        serializer = CopieSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        book = get_object_or_404(Book, pk=request.data["book_id"])
-        serializer.save(book=book)
-        return Response(serializer.data, status.HTTP_201_CREATED)
